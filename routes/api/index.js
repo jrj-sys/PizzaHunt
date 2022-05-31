44d72ae392081d0ad5249e3af8666bd7056ead37
jrj-sys <jamesjamel99@gmail.com>
2022-05-31 05:31:36 -0500

    comments CRUD

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+
+const commentRoutes = require('./comment_routes');
 const pizzaRoutes = require('./pizza_routes');
 
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
